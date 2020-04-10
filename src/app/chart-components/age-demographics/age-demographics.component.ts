6add98c0b7a8a0e91e1abf7aece7964dd8b9a2ce
vignesh1001 <vignesh1001@gmail.com>
2020-04-10 19:14:23 +0530

    2 files modified

--- src/app/chart-components/age-demographics/age-demographics.component.ts
+++ src/app/chart-components/age-demographics/age-demographics.component.ts
@@ -33,28 +33,70 @@ export class AgeDemographics implements OnInit, DoCheck {
 
     this.options = {
       tooltip: {
-        // formatter: params => {
-        //   return '<div style="width:300px; height: 400px">working</div>';
-        // },
         formatter: this.getTooltipFormatter(),
         confine: true
       },
-      xAxis: {
-        data: xAxisData
-      },
-      yAxis: {},
-      series: [
-        {
-          type: 'bar',
-          barCategoryGap: '0%',
-          data: data1
+      series: [{
+        name: 'Age Demographics',
+        type: 'pie',
+        radius: ['40%', '55%'],
+        hoverOffset: 3,
+        color: ['#facd07','#a988d8'],
+        label: {
+          formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+          backgroundColor: '#eee',
+          borderColor: '#aaa',
+          borderWidth: 1,
+          borderRadius: 4,
+          // shadowBlur:3,
+          // shadowOffsetX: 2,
+          // shadowOffsetY: 2,
+          // shadowColor: '#999',
+          // padding: [0, 7],
+          rich: {
+            a: {
+              color: '#999',
+              lineHeight: 22,
+              align: 'center'
+            },
+            abg: {
+              backgroundColor: '#333',
+              width: '100%',
+              align: 'right',
+              height: 22,
+              borderRadius: [4, 4, 0, 0]
+            },
+            hr: {
+              borderColor: '#aaa',
+              width: '100%',
+              borderWidth: 0.5,
+              height: 0
+            },
+            b: {
+              fontSize: 16,
+              lineHeight: 33
+            },
+            per: {
+              color: '#eee',
+              backgroundColor: '#334455',
+              padding: [2, 4],
+              borderRadius: 2
+            }
+          }
         },
-        {
-          type: 'bar',
-          barCategoryGap: '0%',
-          data: data2
+        data: [
+          {value: 75, name: '25%'},
+          {value: 25, name: '75%'},
+        ],
+        emphasis: {
+            itemStyle: {
+                shadowBlur: 2,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
         }
-      ]
+      }
+    ]
     };
   }
 }
